7d7bba68ef4674f041d1f1ee441306d46db46fa2
Felix Breitenbach <felix.breitenbach@gmail.com>
2019-04-17 19:11:54 +0200

    finished exercise 3

--- src/Box.elm
+++ src/Box.elm
@@ -20,3 +20,14 @@ flipBox { a, b, c } =
   { a = add a b 
   , b = neg b 
   , c = c }
+
+tossBox : Box -> Box
+tossBox { a, b, c } =
+  { a = add b c
+          |> add a
+          |> scale 0.5
+  , b = add b c
+          |> scale 0.5
+  , c = sub c b 
+          |> scale 0.5
+  }
